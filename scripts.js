25fe6843f7c0183595932b25d233457e6612a098
Girgis Sami <jirgissamy78@gmail.com>
2025-05-15 22:13:41 +0300

    render education section by js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 // Dom selector
 const nav = document.querySelector("nav");
-
+const educationContainer = document.querySelector(".education-container");
 // Configurations
 const sections = ["about", "skills", "projects", "contact", "cv"];
 const navList = sections.map((l) => `<li><a href="#${l}">${l}</a></li>`);
@@ -18,6 +18,62 @@ window.addEventListener("scroll", () => {
     nav.classList.remove("sticky-nav");
   }
 });
+
+// Education Section
+const computerScience = {
+  school: " Bachelor of Science (B.S.) in Computer Science",
+  schoolName:
+    "   Faculty of Science, Computer Science Department (Minia University)",
+  year: "2021 - 2025",
+  grade: "85",
+};
+const class12th = {
+  school: "Class-12th (H.S.C)",
+  schoolName: "Matai secondary school (Matai State Board) ",
+  year: "2021",
+  grade: "74.2",
+};
+
+const class9th = {
+  school: "Class-9th (E.S.C)",
+  schoolName: "El-Kawady School (Matai State Board)",
+  year: "2018",
+  grade: "96.79",
+};
+const education = [computerScience, class12th, class9th];
+
+const educationHtml = education
+  .map(
+    (edu) => `   <div class="education">
+              <div class="school-certification">
+                ${edu.school}
+              </div>
+              <div class="school-name">
+               ${edu.schoolName}
+              </div>
+              <div class="year-grade">
+                <div class="year">
+                  <img
+                    src="./assets//icons/calendar-days-solid.svg"
+                    class="icon"
+                    alt=""
+                  />${edu.year}
+                </div>
+                <div class="grade">
+                  <img
+                    src="./assets/icons/percent-solid.svg"
+                    class="icon"
+                    alt=""
+                  />${edu.grade}
+                </div>
+              </div>
+            </div>`
+  )
+  .join("");
+
+// render  Education
+
+educationContainer.insertAdjacentHTML("afterbegin", educationHtml);
 
 // Simple smooth scroll behavior
 document.querySelectorAll("a[href^='#']").forEach((link) => {
