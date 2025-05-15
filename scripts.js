a490e75b5919e6f7950893eb83c086f5a025dc6f
Girgis Sami <jirgissamy78@gmail.com>
2025-05-15 21:29:24 +0300

    create navbar with script

--- scripts.js
+++ scripts.js
@@ -1,3 +1,17 @@
+// Dom selector
+const nav = document.querySelectorAll("nav");
+
+// Configurations
+const sections = ["about", "skills", "projects", "contact", "cv"];
+const navList = sections.map((l) => `<li><a href="#${l}">${l}</a></li>`);
+const navListHtml = navList.join("");
+
+// Create Navbar
+nav.forEach((n) => {
+  const ulList = `<ul>${navListHtml}</ul>`;
+  n.insertAdjacentHTML("afterbegin", ulList);
+});
+
 // Simple smooth scroll behavior
 document.querySelectorAll("a[href^='#']").forEach((link) => {
   link.addEventListener("click", function (e) {
